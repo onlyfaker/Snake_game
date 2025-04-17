ca6a8432d0bae686a5eb8c5ec6747425e93a57f4
Gemaljevic Aleksandar <67740316+onlyfaker@users.noreply.github.com>
2025-04-16 21:39:29 -0400

    update, adding classes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #todo/done - 1. create snake body
-#todo - 2. move the snake
+#todo/done - 2. move the snake
 #todo - 3. control snake
 #todo - 4. detect food collision
 #todo - 5. create scoreboard
@@ -7,33 +7,20 @@
 #todo - 7. tail collsion
 import time
 from turtle import Turtle,Screen
-
+from snake import Snake
 screen = Screen()
 screen.setup(width=600,height=600)
 screen.bgcolor('black')
 screen.title('Snake Game')
 screen.tracer(0) #tracer is off
 
-segments = []
-xcor = 0
-for square in range(0,3):
-    body = Turtle(shape='square')
-    body.penup()
-    body.color('white')
-    body.goto(xcor, 0)
-    xcor-=20
-    segments.append(body)
-
+snake=Snake()
 
 game_on = True
 while game_on:
     screen.update()
     time.sleep(0.1)
-    for seg in range(len(segments)-1,0,-1):
-        new_xcor = segments[seg-1].xcor()
-        new_ycor = segments[seg-1].ycor()
-        segments[seg].goto(new_xcor,new_ycor)
-    segments[0].forward(20)
+    snake.move()
 
 
 screen.exitonclick()
---
+++ snake.py
@@ -0,0 +1,26 @@
+from turtle import Turtle
+MOVE_DISTANCE = 20
+
+
+class Snake:
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+    def create_snake(self):
+        xcor = 0
+        for square in range(0, 3):
+            body = Turtle(shape='square')
+            body.penup()
+            body.color('white')
+            body.goto(xcor, 0)
+            xcor -= 20
+            self.segments.append(body)
+
+    def move(self):
+        for seg in range(len(self.segments)-1,0,-1):
+            new_xcor = self.segments[seg-1].xcor()
+            new_ycor = self.segments[seg-1].ycor()
+            self.segments[seg].goto(new_xcor,new_ycor)
+        self.segments[0].forward(MOVE_DISTANCE)
+
+
