2851d636b373c38a81ab20992ad257b70c20d3f9
Gemaljevic Aleksandar <67740316+onlyfaker@users.noreply.github.com>
2025-04-17 21:28:57 -0400

    update

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 #todo/done - 1. create snake body
 #todo/done - 2. move the snake
 #todo/done - 3. control snake
-#todo - 4. detect food collision
+#todo/done - 4. detect food collision
 #todo - 5. create scoreboard
 #todo - 6. wall collsion
 #todo - 7. tail collsion
 import time
-from turtle import Turtle,Screen
-
+from turtle import Screen
 from food import Food
 from snake import Snake
+
 screen = Screen()
 screen.setup(width=600,height=600)
 screen.bgcolor('black')
@@ -17,22 +17,24 @@ screen.title('Snake Game')
 screen.tracer(0) #tracer is off
 food = Food()
 
-snake=Snake()
+snake = Snake()
 screen.listen()
 screen.onkey(snake.up,"Up")
 screen.onkey(snake.down,"Down")
 screen.onkey(snake.left,"Left")
 screen.onkey(snake.right,"Right")
 
-
 game_on = True
 while game_on:
     screen.update()
     time.sleep(0.1)
+    screen.update()
     snake.move()
     screen.update()
 
+    if snake.head.distance(food) <= 15:
+        food.refresh()
 
-
+    screen.update()
 
 screen.exitonclick()
